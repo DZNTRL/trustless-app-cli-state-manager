5898848efb15540ab97715665aa2d04b7c03d638
joshuaebowling <joshuaebowling@gmail.com>
2021-10-09 02:39:08 -0400

    JBOT-52 organize export for state-manager

--- src/index.ts
+++ src/index.ts
@@ -3,14 +3,21 @@ import { IUser } from "pro-web-common/dist/js/interfaces/state-manager/actions/I
 import { IAppState } from "pro-web-common/dist/js/interfaces/state-manager/states/IApp"
 import { IUserState } from "pro-web-common/dist/js/interfaces/state-manager/states/IUser";
 import { Reducer } from "pro-web-common/dist/js/types/state-manager/Reducer"
+import { AppActions } from "./actions/App";
+import { User as UserActions } from "./actions/User";
+import { AppState } from "./initialStates/App";
+import { UserState } from "./initialStates/User";
+import AppReducer from "./reducers/App";
+import UserReducer from "./reducers/User";
 
 export interface IActions {
     app: IApp
-    user: IUser
+    user: (svc) => IUser
 }
 
 export interface IInitialStates {
     app: IAppState
+    user: IUserState
 }
 
 export interface IReducers {
@@ -22,4 +29,23 @@ export interface IStateManager {
     actions: IActions
     states: IInitialStates
     reducers: IReducers
+}
+
+export const actions: IActions = {
+    app: AppActions,
+    user: UserActions
+}
+
+export const reducers: IReducers = {
+    app: AppReducer,
+    user: UserReducer
+}
+
+export const states: IInitialStates = {
+    app: AppState,
+    user: UserState
+}
+
+export const StateManager: IStateManager = {
+    actions, reducers, states
 }
